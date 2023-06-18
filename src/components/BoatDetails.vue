8b16fef6dea0906f101f644cba09f1c9aac6b260
Soenke Sobott <sobott.soenke@gmail.com>
2023-06-18 18:51:15 +0200

    Refactoring BoatDetails

--- src/components/BoatDetails.vue
+++ src/components/BoatDetails.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <button @click="goBack">Back to Overview</button>
+  <div class="container">
+    <button @click="goBack" class="back-button">Back to Overview</button>
     <h1>Boat Details</h1>
-    <div v-if="boat">
-      <p>ID: {{ boat.id }}</p>
-      <p>Name: {{ boat.name }}</p>
-      <p>Description: {{ boat.description }}</p>
+    <div v-if="boat" class="boat-details">
+      <p><strong>ID:</strong> {{ boat.id }}</p>
+      <p><strong>Name:</strong> {{ boat.name }}</p>
+      <p><strong>Description:</strong> {{ boat.description }}</p>
     </div>
 
     <form @submit.prevent="updateBoat" class="boat-form" v-if="boat">
@@ -16,15 +16,28 @@
         type="text"
         placeholder="Boat Description"
       />
-      <button type="submit">Update Boat</button>
+      <button type="submit" class="submit-button">Update Boat</button>
     </form>
   </div>
 </template>
 
 <script>
-import { ref, onBeforeMount } from "vue";
+import { ref, onBeforeMount, watch } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
+
+const authenticate = (router) => {
+  const jwt = localStorage.getItem("jwt");
+  if (!jwt) {
+    router.push("/");
+  }
+  return jwt;
+};
+
+const handleError = (err, router) => {
+  console.error(err);
+  router.push("/");
+};
 
 export default {
   props: {
@@ -34,8 +47,8 @@ export default {
     const router = useRouter();
     const boat = ref(null);
 
-    onBeforeMount(async () => {
-      const jwt = localStorage.getItem("jwt");
+    const fetchBoat = async () => {
+      const jwt = authenticate(router);
       if (jwt) {
         try {
           const response = await axios.get(
@@ -46,16 +59,13 @@ export default {
           );
           boat.value = response.data;
         } catch (err) {
-          console.error(err);
-          router.push("/");
+          handleError(err, router);
         }
-      } else {
-        router.push("/");
       }
-    });
+    };
 
     const updateBoat = async () => {
-      const jwt = localStorage.getItem("jwt");
+      const jwt = authenticate(router);
       if (jwt) {
         try {
           await axios.put(
@@ -66,11 +76,8 @@ export default {
             }
           );
         } catch (err) {
-          console.error(err);
-          router.push("/");
+          handleError(err, router);
         }
-      } else {
-        router.push("/");
       }
     };
 
@@ -78,7 +85,49 @@ export default {
       router.push("/boats");
     };
 
+    watch(() => props.id, fetchBoat, { immediate: true });
+
     return { boat, updateBoat, goBack };
   },
 };
 </script>
+
+
+<style scoped>
+.container {
+  width: 80%;
+  margin: auto;
+  padding-top: 20px;
+}
+
+.back-button,
+.submit-button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.boat-details,
+.boat-form {
+  background-color: #f9f9f9;
+  padding: 20px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+}
+
+.boat-form h2,
+.back-button {
+  margin-bottom: 20px;
+}
+
+.boat-form input {
+  margin-bottom: 10px;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+}
+</style>
